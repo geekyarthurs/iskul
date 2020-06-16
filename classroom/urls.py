b2bbe0e2a6b7729e682ea5eb690c539c85cadcca
Mahesh Regmi <geekyarthurs@gmail.com>
2020-06-16 13:50:36 +0545

    updated courses

--- classroom/urls.py
+++ classroom/urls.py
@@ -1,3 +1,14 @@
 from django.urls import path
 from . import views
-urlpatterns = [path('', views.Dashboard.as_view())]
+
+app_name = 'classroom'
+
+urlpatterns = [
+    path('', views.Dashboard.as_view(), name='dashboard'),
+    path('subject/<int:subject>/chapter/<int:chapter>/',
+         views.ContentPage.as_view(),
+         name='content_page_yes'),
+    path('subject/<int:subject>/',
+         views.ContentPage.as_view(),
+         name='content_page_no'),
+]
--- classroom/views.py
+++ classroom/views.py
@@ -3,15 +3,47 @@ from django import views
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # Create your views here.
 from django.shortcuts import resolve_url
+from .models import Grade, Content, Subject, Chapter
 
 
 class Dashboard(LoginRequiredMixin, UserPassesTestMixin, views.View):
 
-    template_url = "classroom/index.html"
+    template_url = "classroom/subjects_page.html"
 
     def test_func(self):
         login_url = "accounts:login"
         return self.request.user.is_student
 
     def get(self, request):
-        return render(request, self.template_url)
+        grade = request.user.student.grades
+        className = grade.className
+
+        subjects = grade.subject_set.all()
+
+        context = {'grade': className, 'subjects': subjects}
+
+        return render(request, self.template_url, context)
+
+
+class ContentPage(LoginRequiredMixin, UserPassesTestMixin, views.View):
+    template_url = "classroom/content_page.html"
+
+    def test_func(self):
+        login_url = "accounts:login"
+        return self.request.user.is_student
+
+    def get(self, request, subject, chapter=None):
+
+        chapters = Subject.objects.get(pk=subject).chapter_set.all()
+
+        print(chapters)
+
+        if chapter == None:
+            content = Chapter.objects.all().first().content_set.all()
+        else:
+            content = Chapter.objects.get(pk=chapter).content_set.all()
+
+        context = {'chapters': chapters, 'contents': content}
+        # content =
+
+        return render(request, self.template_url, context)
