4d9e14fb009fde35dc987505408d05112acc3171
ubuntu <rupeshacharya2014@gmail.com>
2020-06-20 12:56:13 +0545

    UI updated

--- admission/admin.py
+++ admission/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import Admission
 # Register your models here.
 
+class admissionModel(admin.ModelAdmin):
+    list_display = ('student_name', 'class_name','student_address','student_contact','date_of_admission', )
 
-admin.site.register(Admission)
+admin.site.register(Admission, admissionModel)
