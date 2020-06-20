803ff895ec2905192ff58dca710fc2dcbba9a6aa
Mahesh Regmi <geekyarthurs@gmail.com>
2020-06-20 09:42:05 +0545

    requirements and static

--- Iskul/settings.py
+++ Iskul/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Custom user model
 AUTH_USER_MODEL = 'accounts.MyUser'
