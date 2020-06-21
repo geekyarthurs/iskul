9d43bf0bb0ae1333818d6f076393b4c5fece71d3
ubuntu <rupeshacharya2014@gmail.com>
2020-06-22 02:32:48 +0545

    Pagination added in Announcement

--- announcement/views.py
+++ announcement/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from announcement.models import Announcement
-
+from django.core.paginator import Paginator
 from django.db.models import Q
 
 # Create your views here.
@@ -11,11 +11,23 @@ def announcement(request):
     else:
         
         grade = request.user.teacher.grades
-        
-    announcement = Announcement.objects.filter(
-        Q(announcement_type="Public") | Q(class_name=grade)).order_by('-date_announced')[:10]
+
+    announcement_list = Announcement.objects.filter(
+        Q(announcement_type="Public") | Q(class_name=grade)).order_by('-date_announced')
+
+
+    page = request.GET.get('page', 1)    
+    paginator = Paginator(announcement_list, 3)
+    try:
+        announcements = paginator.page(page)
+    except PageNotAnInteger:
+        announcements = paginator.page(1)
+    except EmptyPage:
+        announcements = paginator.page(paginator.num_pages)
+
+
     context = {
-        'announcements' : announcement,
+        'announcements' : announcements,
     }
     print(announcement)
     return render(request , 'announcement/index.html', context)
